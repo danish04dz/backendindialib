78c16067de0c786ffdd9c5598ccaa25c6504a5d8
Md Danish <mddanish04dz@gmail.com>
2024-09-09 01:57:28 +0530

    index fileupdated

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const express = require("express");
 const cors = require("cors");
 const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
-
-require('./utils/expiredBookings');
-
-const app = express();
-
 require('dotenv').config();
+require('./utils/expiredBookings');
+const cookieParser = require("cookie-parser");
+const app = express();
 const PORT = process.env.PORT || 4000;
 
-const cookieParser = require("cookie-parser");
+// Middleware to parse cookies
 app.use(cookieParser());
 
-
-
+// Allowed Origins for CORS
 const allowedOrigins = [
   "http://localhost:5173",  // For local development
   "https://indialib.netlify.app"  // Netlify production frontend
 ];
 
+// CORS options to handle allowed origins
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.includes(origin) || !origin) {
@@ -28,26 +26,28 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true,
+  credentials: true,  // Enable cookies and other credentials
 };
 
+// Apply CORS middleware with options
 app.use(cors(corsOptions));
 
+// Middleware to parse JSON
+app.use(express.json());
 
-require("./config/database").connect();
-
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
-  })
-);
+// Middleware for file uploads using Cloudinary
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: "/tmp/",
+}));
 
 // Connecting to Cloudinary
 cloudinaryConnect();
-app.get("/",(req,res)=>{
-  res.send("tezting production api")
-})
+
+// Simple route to test if the server is running
+app.get("/", (req, res) => {
+  res.send("Testing production API");
+});
 
 // Import and mount routes
 const user = require("./routes/user");
@@ -61,6 +61,9 @@ app.use("/api/v1/", libraryRoutes);
 
 const bookings = require("./routes/booking");
 app.use("/api/v1/bookings", bookings);
+
+// Connect to the database
+require("./config/database").connect();
 
 // Activate server
 app.listen(PORT, () => {
